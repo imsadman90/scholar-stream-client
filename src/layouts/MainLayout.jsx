39c742402c8b0268361333fd65cdcf34792d1735
imsadman90 <sadmansam473@gmail.com>
2025-12-14 23:38:58 +0600

    main layout has been completed

---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,22 @@
+import { Outlet } from "react-router-dom";
+import NavBar from "../components/shared/NavBar";
+import Footer from "../components/shared/Footer";
+import ScrollToTop from "../components/ScrolToTop/ScrolToTop";
+
+
+const MainLayout = () => {
+  return (
+    <>
+      <div className="min-h-screen flex flex-col">
+        <NavBar />
+        <main className="flex-1">
+          <ScrollToTop/>
+          <Outlet />
+          <Footer />
+        </main>
+      </div>
+    </>
+  );
+};
+
+export default MainLayout;
