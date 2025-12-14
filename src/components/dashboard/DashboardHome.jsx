631832347f1e8176006f73a679c5c0c1f8ea220e
imsadman90 <sadmansam473@gmail.com>
2025-12-14 23:44:37 +0600

    conditional dashboard home

---
+++ src/components/dashboard/DashboardHome.jsx
@@ -0,0 +1,25 @@
+
+import ManageApplications from "./moderator/ManageApplications";
+import Dashboard from "../../pages/Dashboard"; // ← Correct import
+import useRole from "../../hooks/useRole";
+import LoadingSpinner from "../shared/Loading";
+
+const DashboardHome = () => {
+  const [role, isRoleLoading] = useRole();
+
+  if (isRoleLoading) {
+    return <LoadingSpinner />;
+  }
+
+  if (role === "admin") {
+    return <Dashboard/>;
+  }
+
+  if (role === "moderator") {
+    return <Dashboard/>; 
+  }
+
+  return <Dashboard />;
+};
+
+export default DashboardHome;
