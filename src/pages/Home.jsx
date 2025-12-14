3a510ef78edf8e74bb54f82e032fb7c1717a7ab7
imsadman90 <sadmansam473@gmail.com>
2025-12-15 00:14:47 +0600

    home jsx issue solved

---
+++ src/pages/Home.jsx
@@ -0,0 +1,28 @@
+import Banner from "../components/Home/Banner";
+import FAQ from "../components/Home/FAQ";
+import TopScholarships from "../components/Home/TopScholarships";
+import SuccessStories from "../components/Home/SuccessStories";
+import { useLocation } from "react-router";
+import { useEffect } from "react";
+
+const Home = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.state?.scrollTo === "contact") {
+      const contactSection = document.getElementById("contact");
+      contactSection?.scrollIntoView({ behavior: "smooth" });
+    }
+  }, [location]);
+
+  return (
+    <div>
+      <Banner />
+      <TopScholarships />
+      <SuccessStories />
+      <FAQ />
+    </div>
+  );
+};
+
+export default Home;
